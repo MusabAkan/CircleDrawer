12e2e40ecb618cc60ffde13e53156307df8bf05e
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-10-22 13:35:15 +0300

    Add project files.

---
+++ CircleDrawer.cs
@@ -0,0 +1,20 @@
+﻿namespace CircleDrawer
+{
+    class CircleDrawer
+    {
+        public static void DrawCircle(int radius)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                for (int x = -radius; x <= radius; x++)
+                {
+                    if (x * x + y * y <= radius * radius)
+                        Console.Write("* ");
+                    else
+                        Console.Write(string.Empty); // Boşluk karakteri
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Dairenin yarıçapını girin: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            if (radius <= 0)
+            {
+                Console.WriteLine("Geçersiz yarıçap. Pozitif bir tamsayı girin.");
+                return;
+            }
+
+            CircleDrawer.CircleDrawer.DrawCircle(radius);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş. Bir tamsayı girin.");
+        }
+    }
+}
